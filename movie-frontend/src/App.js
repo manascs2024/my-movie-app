7de112c22885ff010bf52c9d8f9176a39d6a6713
Manas Pankaj(Intern) <manas.pankaj@ohmium.com>
2025-05-08 17:13:30 +0530

    login css not working

--- movie-frontend/src/App.js
+++ movie-frontend/src/App.js
@@ -11,7 +11,7 @@ import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
 import ProtectedRoute from "./components/ProtectedRoute";
 import './App.css';
-import './css/regislog.css'
+import './css/regislog.css';
 
 
 function App() {
